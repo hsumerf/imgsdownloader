66f6986eac7532eb3575523bfe2f9b4f1ad2309d
H S Umer Farooq <h.s.umer.farooq@gmail.com>
2018-07-11 15:14:57 +0500

    after making login of linkedin with scrapy and selenium

--- imgsdownloader/spiders/linkedin.py
+++ imgsdownloader/spiders/linkedin.py
@@ -1,13 +1,21 @@
 # -*- coding: utf-8 -*-
 import scrapy
-from selenium import webdriver
-
+    from selenium import webdriver
+from time import sleep
 class LinkedinSpider(scrapy.Spider):
     name = 'linkedin'
     allowed_domains = ['linkedin.com']
     def start_requests(self):
         driver = webdriver.Chrome('/home/hsumerf/Desktop/chromedriver')
         driver.get('https://www.linkedin.com')
+        sleep(3)
+        #page_source = driver.page_source
+        email = driver.find_element_by_class_name('login-email')
+        email.send_keys('h.s.umer.farooq@gmail.com')
+        password = driver.find_element_by_class_name('login-password')
+        password.send_keys('cscourse+-789')
+        submit = driver.find_element_by_xpath('//*[@type="submit"]')
+        submit.click()
 
 
 
